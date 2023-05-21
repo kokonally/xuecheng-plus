10d78620d96233d0ac7eaaa5bd8bf3e649839044
kokonally <1195218191@qq.com>
2023-05-21 19:12:35 +0800

    完善课程预览的接口文档

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
@@ -2,17 +2,21 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CoursePreviewDto;
 import com.xuecheng.content.service.CoursePublishService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+@Api(value = "课程发布相关接口", tags = "课程发布相关接口")
 @Controller
 public class CoursePublishController {
     @Autowired
     private CoursePublishService coursePublishService;
 
+    @ApiOperation("课程预览页面返回")
     @GetMapping("/coursepreview/{courseId}")
     public ModelAndView preview(@PathVariable("courseId") Long courseId) {
         ModelAndView modelAndView = new ModelAndView();
